3b7b94ed7d976eb39cdc63dd09070e86220af0f1
diaoyufeng <diaoyufeng@axinfu.com>
2022-07-11 17:15:10 +0800

    feat: add multiply and divide calc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 const calc = (a: number, b: number, type?: string): number => {
   switch (type) {
+    case 'mutiply':
+      return a * b
+    case 'divide':
+      return a / b
     case 'minus':
       return a - b
     default:
